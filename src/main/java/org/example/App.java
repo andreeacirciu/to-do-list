7b9b0d8fba5030000d4a9df2a9f83b77c5d0d81d
andreeacirciu <andreeacirciu@gmail.com>
2020-05-24 15:29:44 +0300

    implemented CRUD methods in task repository

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.domain.Task;
 import org.example.persistance.TaskRepository;
 import org.example.transfer.CreateTaskRequest;
+import org.example.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Hello world!
@@ -13,12 +16,22 @@ import java.time.LocalDate;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException, SQLException {
+    public static void main( String[] args ) throws IOException, SQLException, ClassNotFoundException {
         CreateTaskRequest request = new CreateTaskRequest();
         request.setDescription("Learn JDBC");
         request.setDeadLine(LocalDate.now().plusWeeks(2));
 
         TaskRepository taskRepository = new TaskRepository();
         taskRepository.createTask(request);
+
+//        UpdateTaskRequest updateRequest = new UpdateTaskRequest();
+//        updateRequest.setDone(true);
+//
+//        taskRepository.updateTask(1, updateRequest);
+
+//        taskRepository.deleteTask(2);
+
+        List<Task> tasks = taskRepository.getTasks();
+        System.out.println(tasks);
     }
 }
--- src/main/java/org/example/persistance/TaskRepository.java
+++ src/main/java/org/example/persistance/TaskRepository.java
@@ -1,12 +1,15 @@
 package org.example.persistance;
 
+import org.example.domain.Task;
 import org.example.transfer.CreateTaskRequest;
+import org.example.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.CheckedOutputStream;
 
 public class TaskRepository {
 
@@ -24,5 +27,54 @@ public class TaskRepository {
 
             preparedStatement.executeUpdate();
         }
+    }
+
+    public void updateTask(long id, UpdateTaskRequest request) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE task SET done=? WHERE id=?";
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setBoolean(1, request.isDone());
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+
+        }
+    }
+
+    public void deleteTask(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM task WHERE id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<Task> getTasks() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, description, deadline, done FROM task";
+
+        List<Task> tasks = new ArrayList<>();
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+           while(resultSet.next()){
+               Task task = new Task();
+               task.setId(resultSet.getLong("id"));
+               task.setDescription(resultSet.getString("description"));
+
+               Date deadlineAsSqlDate = resultSet.getDate("deadline");
+               task.setDeadline(deadlineAsSqlDate.toLocalDate());
+
+               task.setDone(resultSet.getBoolean("done"));
+
+               tasks.add(task);
+           }
+        }
+        return tasks;
     }
 }
